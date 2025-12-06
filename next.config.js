2d83aff2edc7ee5efbf8b5104c0eceaa25409b3f
inate-13 <chouhan13sagar@gmail.com>
2025-12-06 09:14:00 +0530

    fix; vercel.json added ,next config changed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
+// /**
+//  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+//  * for Docker builds.
+//  */
+// import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+// /** @type {import("next").NextConfig} */
+// const config = {};
 
-export default config;
+// export default config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      // add other image hosts you use
+    ],
+  },
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
